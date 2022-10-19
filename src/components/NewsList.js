f0f7b3db12e5034c00b77853bbbbce6ad4858181
hsh <hanssangh@naver.com>
2022-10-20 00:01:48 +0900

    커스텀 Hook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from 'axios';
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -24,33 +25,24 @@ const sampleArticle = {
 };
 
 const NewsList = ({category}) => {
-    const[articles, setArticles] = useState(null);
-    const[loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c98aed2a201547b195726ea32d4d3b98`
-                );
-                setArticles(response.data.articles);
-            } catch(e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c98aed2a201547b195726ea32d4d3b98`
+        );
     }, [category]);
 
     if(loading) {
         return <NewsListBlock>대기 중...</NewsListBlock>
     }
-    if(!articles) {
+    if(!response) {
         return null;
     }
+    if(error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
 
+    const {articles} = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
