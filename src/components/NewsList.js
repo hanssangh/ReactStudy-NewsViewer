8f80624c496bd373d3947f7e3b24fcff6ba23ec5
hsh <hanssangh@naver.com>
2022-10-19 20:26:20 +0900

    newsapi로 불러온 기사 입력

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -22,14 +24,37 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+    const[articles, setArticles] = useState(null);
+    const[loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=c98aed2a201547b195726ea32d4d3b98'
+                );
+                setArticles(response.data.articles);
+            } catch(e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if(loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    if(!articles) {
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     );
 };
